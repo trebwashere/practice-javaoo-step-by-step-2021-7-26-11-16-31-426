b2bbed54152307df562dc5dcf2e756c41d2e4be8
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-04 23:58:08 +0800

    [STJavaOOP][Robert] feat: Finished practice07 requirements

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private final Integer number;
+    private Student studentLeader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
+
+    public void assignLeader(Student studentLeader) {
+        this.studentLeader = studentLeader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,31 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        String personIntro = super.introduce();
+        return klass != null ? personIntro + " " + String.format("I am a Teacher. I teach %s.", klass.getDisplayName())
+                : personIntro + " " + "I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        String teacherIntro = super.introduce() + " " + "I am a Teacher. ";
+        String studentName = student.getName();
+        return student.getKlass().getNumber().equals(this.getKlass().getNumber()) ?
+                teacherIntro + String.format("I teach %s.", studentName) : teacherIntro + String.format("I don't teach %s.", studentName);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
