0348068f1a72f5e0348645f00e611a2883ed5291
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-04 23:57:18 +0800

    [STJavaOOP][Robert] feat: Finished practice06 requirements

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,17 @@
 package practice06;
 
 public class Klass {
+    private final Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        String personIntro = super.introduce();
+        return klass != null ? personIntro + " " + String.format("I am a Teacher. I teach %s.", klass.getDisplayName())
+                : personIntro + " " + "I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        String teacherIntro = super.introduce() + " " + "I am a Teacher. ";
+        String studentName = student.getName();
+        return student.getKlass().getNumber().equals(this.getKlass().getNumber()) ?
+                teacherIntro + String.format("I teach %s.", studentName) : teacherIntro + String.format("I don't teach %s.", studentName);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
