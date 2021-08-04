ed3774c143e468ac379fd35b72b7efddaf558e7c
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 00:00:09 +0800

    [STJavaOOP][Robert] feat: Tried doing observer pattern, unfortunately didnt work

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,50 @@
 package practice10;
 
-public class Klass {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+public class Klass extends Observable {
+    private final Integer number;
+    private Student studentLeader;
+    private List<Student> klassStudents = new ArrayList<>();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
+
+    public void assignLeader(Student studentLeader) {
+        if (klassStudents.contains(studentLeader)) {
+            this.studentLeader = studentLeader;
+            notifyObservers();
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public List<Student> getKlassStudents() {
+        return klassStudents;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void appendMember(Student student) {
+        klassStudents.add(student);
+    }
+
+    @Override
+    public void notifyObservers(){
+        setChanged();
+        super.notifyObservers();
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,58 @@
 package practice10;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person implements Observer {
+    private List<Klass> klasses = new ArrayList<>();
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, List<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        String personIntro = super.introduce();
+        return !klasses.isEmpty() ? personIntro + " " + String.format("I am a Teacher. I teach Class %s.",
+                klasses.stream().map(klass -> klass.getNumber().toString()).collect(Collectors.joining(", ")))
+                : personIntro + " " + "I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        String teacherIntro = super.introduce() + " " + "I am a Teacher. ";
+        String studentName = student.getName();
+        return this.getClasses().stream()
+                .map(Klass::getNumber)
+                .collect(Collectors.toList()).contains(student.getKlass().getNumber()) ?
+                teacherIntro + String.format("I teach %s.", studentName) : teacherIntro + String.format("I don't teach %s.", studentName);
+    }
+
+    public List<Klass> getClasses() {
+        return klasses;
+    }
+
+    public boolean isTeaching(Student student) {
+        return klasses.stream()
+                .map(Klass::getNumber)
+                .collect(Collectors.toList())
+                .stream()
+                .anyMatch(klass -> klass.equals(student.getKlass().getNumber()));
+
+    }
+
+    @Override
+    public void update(Observable observable, Object arg)
+    {
+        klass = (Klass) observable;
+        System.out.printf("I am %s. I know %s become Leader of Class %d%n", getName(), klass.getLeader().getName(), klass.getNumber());
+    }
+
 }
