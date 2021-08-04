a70fc990a0b0604bb9e108b304780285cf458422
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-04 23:55:35 +0800

    [STJavaOOP][Robert] feat: Finished practice04 requirements

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " " + String.format("I am a Student. I am at Class %d.", klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
