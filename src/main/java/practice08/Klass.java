708383006a11609b21666b5d503bc9fd61b768fb
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-04 23:58:42 +0800

    [STJavaOOP][Robert] feat: Finished practice08 requirements

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,39 @@
 package practice08;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private final Integer number;
+    private Student studentLeader;
+    private List<Student> klassStudents = new ArrayList<>();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
+
+    public void assignLeader(Student studentLeader) {
+        if (klassStudents.contains(studentLeader)) {
+            this.studentLeader = studentLeader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void appendMember(Student student) {
+        klassStudents.add(student);
+    }
 }
