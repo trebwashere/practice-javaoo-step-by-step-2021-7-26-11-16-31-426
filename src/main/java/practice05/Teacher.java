1def01bfd6be8f3a4f477187e9708f69a4144456
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-04 23:56:31 +0800

    [STJavaOOP][Robert] feat: Finished practice05 requirements

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,23 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        String teacherIntro = super.introduce() + " " + "I am a Teacher. ";
+        return klass != null ? teacherIntro + String.format("I teach Class %d.", klass) : teacherIntro + "I teach No Class.";
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
